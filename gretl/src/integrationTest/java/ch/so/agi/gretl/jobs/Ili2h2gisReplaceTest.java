84350aa9da2beb082200f0741fd39a255dcb68b1
Stefan Ziegler <edi.gonzales@gmail.com>
2021-02-27 19:16:09 +0100

    add ili2h2gis replace task

--- gretl/src/integrationTest/java/ch/so/agi/gretl/jobs/Ili2h2gisReplaceTest.java
+++ gretl/src/integrationTest/java/ch/so/agi/gretl/jobs/Ili2h2gisReplaceTest.java
@@ -14,27 +14,27 @@ import org.locationtech.jts.geom.Point;
 
 import ch.so.agi.gretl.util.IntegrationTestUtil;
 
-public class Ili2h2gisImportTest {
+public class Ili2h2gisReplaceTest {
     @Test
-    public void import_Ok() throws Exception {
+    public void replace_Ok() throws Exception {
         // Prepare
-        String[] files = new File("src/integrationTest/jobs/Ili2h2gisImport/").list();
+        String[] files = new File("src/integrationTest/jobs/Ili2h2gisReplace/").list();
         for (String file : files) {
             if (file.contains("db")) {
-                Paths.get("src/integrationTest/jobs/Ili2h2gisImport/", file).toFile().delete();
+                Paths.get("src/integrationTest/jobs/Ili2h2gisReplace/", file).toFile().delete();
             }
         }
         
         // Run GRETL job
-        IntegrationTestUtil.runJob("src/integrationTest/jobs/Ili2h2gisImport");
+        IntegrationTestUtil.runJob("src/integrationTest/jobs/Ili2h2gisReplace");
         
         // Check results
-        String testDbFileName = new File("src/integrationTest/jobs/Ili2h2gisImport/254900").getAbsolutePath();
+        String testDbFileName = new File("src/integrationTest/jobs/Ili2h2gisReplace/254900").getAbsolutePath();
         String testDbUrl = "jdbc:h2:" + testDbFileName;
         try (Connection conn = DriverManager.getConnection(testDbUrl); Statement stmt = conn.createStatement()) {
-            ResultSet rs = stmt.executeQuery("SELECT nummer, geometrie, hoehegeom FROM LFP3 WHERE nummer = '11075350'");
+            ResultSet rs = stmt.executeQuery("SELECT nummer, geometrie, hoehegeom FROM LFP3 WHERE nummer = '99999999'");
             while (rs.next()) {
-                Assert.assertEquals("11075350", rs.getString(1));
+                Assert.assertEquals("99999999", rs.getString(1));
                 Assert.assertEquals(2611985.735, ((Point)rs.getObject(2)).getX(), 0.001);
                 Assert.assertEquals(584.330, rs.getDouble(3), 0.001);
             }
