92fe079f76441abf9884fac205ef607aa44ea0f4
claeis <ce@eisenhutinformatik.ch>
2022-02-03 09:47:42 +0100

    fix failing test

--- gretl/src/test/java/ch/so/agi/gretl/testutil/FileListTest.java
+++ gretl/src/test/java/ch/so/agi/gretl/testutil/FileListTest.java
@@ -1,16 +1,11 @@
 package ch.so.agi.gretl.testutil;
 
-import java.io.File;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import ch.interlis.iox.IoxReader;
 import ch.so.agi.gretl.steps.AbstractPublisherStepTest;
 import ch.so.agi.gretl.steps.PublisherStep;
 
@@ -23,12 +18,4 @@ public class FileListTest {
         Assert.assertTrue(regions.contains("2502"));
     }
 
-    @Test
-    public void openFileTest() throws Exception {
-        Path dataFile=Paths.get("build/out/av_test.itf");
-        //List<String> modelnameFromFile = ch.interlis.iox_j.IoxUtility.getModels(dataFile.toFile());
-        IoxReader reader = new ch.interlis.iox_j.utility.ReaderFactory().createReader(dataFile.toFile(), null);
-        reader.close();
-        Files.delete(dataFile);
-    }
 }
