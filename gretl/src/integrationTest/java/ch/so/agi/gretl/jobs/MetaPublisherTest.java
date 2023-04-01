89d83f7306b7b055b75d392705dcbf276560473c
Stefan Ziegler <edi.gonzales@gmail.com>
2023-04-01 18:00:58 +0200

    Metapublisher PoC (#111)
    * [MetaPublisher] initial commit
    
    * [MetaPublisher] wip
    
    * [MetaPublisher] start with task
    
    * [MetaPublisher] start with int test
    
    * [MetaPublisher] complete simple use case
    
    * [MetaPublisher] start with regions support
    
    * [MetaPublisher] fix task if region is null
    
    * emulate single repo approach
    
    * clean code
    
    * [MetaPublisher] update geojson with regions update date
    
    * [MetaPublisher] add json update test for step
    
    * [MetaPublisher] regions test for task
    
    * [MetaPublisher] implement fileformats
    
    * WIP
    
    * [MetaPublisher] add geocat export and upload
    
    * [MetaPublisher] clean code
    
    * [MetaPublisher] add missing template file
    
    * [MetaPublisher] some refactoring
    
    * [MetaPublisher] add example with static regions
    
    * [MetaPublisher] clean code
    
    * [MetaPublisher] clean code
    
    * [MetaPublisher] clean code
    
    * [MetaPublisher] clean code
    
    * [MetaPublisher] disable s3 tests
    
    * [MetaPublisher] refactoring for existing gretl jobs structure
    
    * [MetaPublisher] enable s3 tests again

---
+++ gretl/src/integrationTest/java/ch/so/agi/gretl/jobs/MetaPublisherTest.java
@@ -0,0 +1,123 @@
+package ch.so.agi.gretl.jobs;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
+import ch.so.agi.gretl.util.GradleVariable;
+import ch.so.agi.gretl.util.IntegrationTestUtil;
+
+public class MetaPublisherTest {
+    public static final String PATH_ELE_ROOT = "build";
+    public static final String PATH_ELE_AKTUELL = "aktuell";
+    public static final String PATH_ELE_META = "meta";
+    public static final String PATH_ELE_CONFIG = "config";
+    public static final String GEOCAT_FTP_DIR = "geocat";
+    public static final String GEOCAT_PATH_ELE_ENV = "int"; // TODO: beim Testen muss ich es wohl eh hardcodieren? Hier ja, aber dem Job irgend eine ENV-Variable uebergeben oder so. Andi fragen.
+
+    @Test
+    public void simple_Ok() throws Exception {
+        // Prepare
+        String jobDirectory = "src/integrationTest/jobs/MetaPublisher/afu_abbaustellen_pub";
+        String dataIdent = "ch.so.afu.abbaustellen";
+                
+        // Run task
+        GradleVariable[] gvs = null;
+        IntegrationTestUtil.runJob(jobDirectory, gvs);
+        
+        // Check result
+        Path target = Paths.get(jobDirectory);
+        File htmlFile = target.resolve(PATH_ELE_ROOT).resolve(dataIdent).resolve(PATH_ELE_AKTUELL).resolve(PATH_ELE_META).resolve("meta-"+dataIdent+".html").toFile();
+        assertTrue(htmlFile.exists());
+        {
+            byte[] bytes = Files.readAllBytes(htmlFile.toPath());
+            String fileContent = new String (bytes);
+            assertTrue(fileContent.contains("Datenbeschreibung • Amt für Geoinformation Kanton Solothurn"));
+            assertTrue(fileContent.contains("<div id=\"title\">Abbaustellen</div>"));   
+        }
+        
+        File xtfFile = target.resolve(PATH_ELE_ROOT).resolve(PATH_ELE_CONFIG).resolve("meta-"+dataIdent+".xtf").toFile();
+        assertTrue(xtfFile.exists());
+        
+        File xmlFile = target.resolve(PATH_ELE_ROOT).resolve(GEOCAT_FTP_DIR).resolve(GEOCAT_PATH_ELE_ENV).resolve(dataIdent+".xml").toFile();
+        assertTrue(xmlFile.exists());
+        {
+            byte[] bytes = Files.readAllBytes(xmlFile.toPath());
+            String fileContent = new String (bytes);
+            assertTrue(fileContent.contains("<gco:CharacterString>Abbaustellen</gco:CharacterString>"));
+            assertTrue(fileContent.contains("files-i.geo.so.ch"));
+        }
+    }
+    
+    @Test
+    public void regions_Ok() throws Exception {
+        // Prepare
+        String jobDirectory = "src/integrationTest/jobs/MetaPublisher/agi_dm01so_pub";
+        String dataIdent = "ch.so.agi.av.dm01_so";
+        
+        // Run task
+        GradleVariable[] gvs = null;
+        IntegrationTestUtil.runJob(jobDirectory, gvs);
+        
+        // Check result
+        Path target = Paths.get(jobDirectory);
+        File htmlFile = target.resolve(PATH_ELE_ROOT).resolve(dataIdent).resolve(PATH_ELE_AKTUELL).resolve(PATH_ELE_META).resolve("meta-"+dataIdent+".html").toFile();
+        assertTrue(htmlFile.exists());
+        
+        {
+            byte[] bytes = Files.readAllBytes(htmlFile.toPath());
+            String fileContent = new String (bytes);
+            assertTrue(fileContent.contains("Datenbeschreibung • Amt für Geoinformation Kanton Solothurn"));
+            assertTrue(fileContent.contains("<div id=\"title\">Amtliche Vermessung (DM01 CH + DXF/Geobau)</div>"));            
+        }
+
+        File xtfFile = target.resolve(PATH_ELE_ROOT).resolve(PATH_ELE_CONFIG).resolve("meta-"+dataIdent+".xtf").toFile();
+        assertTrue(xtfFile.exists());
+
+        File jsonFile = target.resolve(PATH_ELE_ROOT).resolve(PATH_ELE_CONFIG).resolve(dataIdent + ".json").toFile();
+        assertTrue(jsonFile.exists()); 
+        
+        {
+            byte[] bytes = Files.readAllBytes(jsonFile.toPath());
+            String fileContent = new String (bytes);
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            String formattedDate = sdf.format(new Date());
+
+            assertTrue(fileContent.contains(formattedDate));
+        }
+    }
+    
+    @Test
+    public void static_regions_Ok() throws Exception {
+        // Prepare
+        String jobDirectory = "src/integrationTest/jobs/MetaPublisher/agi_orthofoto_1993_meta_pub";
+        String dataIdent = "ch.so.agi.orthofoto_1993.grau";
+        
+        // Run task
+        GradleVariable[] gvs = null;
+        IntegrationTestUtil.runJob(jobDirectory, gvs);
+
+        // Check result
+        Path target = Paths.get(jobDirectory);
+        
+        File xtfFile = target.resolve(PATH_ELE_ROOT).resolve(PATH_ELE_CONFIG).resolve("meta-"+dataIdent+".xtf").toFile();
+        assertTrue(xtfFile.exists());
+        
+        {
+            byte[] bytes = Files.readAllBytes(xtfFile.toPath());
+            String fileContent = new String (bytes);
+            assertTrue(fileContent.contains("<identifier>2612519_1254998</identifier>"));
+        }
+
+        File jsonFile = target.resolve(PATH_ELE_ROOT).resolve(PATH_ELE_CONFIG).resolve(dataIdent + ".json").toFile();
+        assertTrue(jsonFile.exists()); 
+    }
+}
---
+++ gretl/src/main/java/ch/so/agi/gretl/steps/metapublisher/meta/RegionsUtil.java
@@ -0,0 +1,144 @@
+package ch.so.agi.gretl.steps.metapublisher.meta;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+
+import ch.interlis.iom.IomObject;
+import ch.interlis.iom_j.Iom_jObject;
+
+import ch.so.agi.gretl.logging.GretlLogger;
+import ch.so.agi.gretl.logging.LogEnvironment;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class RegionsUtil {
+    private GretlLogger log = LogEnvironment.getLogger(this.getClass());;
+    
+    private static final String ITEM_STRUCTURE_TAG = "SO_AGI_Metadata_20230304.Item";
+
+    public static void updateJson(File jsonFile, Map<String,String> regionMap) throws JsonParseException, JsonMappingException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(jsonFile);
+       
+        JsonNode features = rootNode.get("features");
+        Iterator<JsonNode> feati = features.iterator();
+        while(feati.hasNext()) {
+            JsonNode feature = feati.next();
+            JsonNode properties = feature.get("properties");
+            JsonNode identifier = properties.get("identifier");
+            String identifierValue = identifier.asText();
+            
+            if (regionMap.get(identifierValue) != null) {
+                ((ObjectNode)properties).put("lastPublishingDate", regionMap.get(identifierValue));
+            }
+        }
+        
+        try (FileWriter file = new FileWriter(jsonFile)) {
+            file.write(rootNode.toString());
+        }        
+    }
+    
+    // TODO: boundary und geometry -> verheiraten mit getBoundary
+    public static void getItems(File jsonFile, List<IomObject> items) throws JsonProcessingException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(jsonFile);
+        JsonNode features = rootNode.get("features");
+        Iterator<JsonNode> feati = features.iterator();
+        while(feati.hasNext()) {
+            JsonNode feature = feati.next();
+            JsonNode properties = feature.get("properties");
+            JsonNode identifier = properties.get("identifier");
+            JsonNode title = properties.get("title");
+            JsonNode lastPublishingDate = properties.get("lastPublishingDate");
+            
+            Iom_jObject itemObj = new Iom_jObject(ITEM_STRUCTURE_TAG, null);
+            itemObj.setattrvalue("identifier", identifier.asText());
+            itemObj.setattrvalue("title", title.asText());
+            itemObj.setattrvalue("lastPublishingDate", lastPublishingDate.asText());
+            items.add(itemObj);
+        }
+    }
+    
+    public static void getBoundary(File jsonFile, Map<String, Double> boundary) throws JsonProcessingException, IOException {
+        Double westlimit = null;
+        Double southlimit = null;
+        Double eastlimit = null;
+        Double northlimit = null;
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(jsonFile);
+        JsonNode features = rootNode.get("features");
+        Iterator<JsonNode> feati = features.iterator();
+        while(feati.hasNext()) {
+            JsonNode feature = feati.next();
+            JsonNode properties = feature.get("properties");
+            JsonNode identifier = properties.get("identifier");
+            JsonNode title = properties.get("title");
+            JsonNode geometry = feature.get("geometry");
+            JsonNode type = geometry.get("type");
+            
+            List<JsonNode> exteriorRings = new ArrayList<JsonNode>();
+            if (type.asText().equalsIgnoreCase("MultiPolygon")) {
+                JsonNode coordinates = geometry.get("coordinates");
+                for (int i=0;i<coordinates.size();i++) {
+                    JsonNode polygon = coordinates.get(0);
+                    JsonNode exteriorRing = polygon.get(0);
+                    exteriorRings.add(exteriorRing);
+                }
+            } else {
+                JsonNode coordinates = geometry.get("coordinates");
+                JsonNode exteriorRing = coordinates.get(0);
+                exteriorRings.add(exteriorRing);
+            }
+            
+            for (JsonNode exteriorRing : exteriorRings) {
+                Iterator<JsonNode> coordi = exteriorRing.iterator();
+                while (coordi.hasNext()) {
+                    JsonNode coordinate = coordi.next();
+                    double easting = coordinate.get(0).asDouble();
+                    double northing = coordinate.get(1).asDouble();
+
+                    if (westlimit == null) {
+                        westlimit = easting;
+                    }
+
+                    if (southlimit == null) {
+                        southlimit = northing;
+                    }
+
+                    if (eastlimit == null) {
+                        eastlimit = easting;
+                    }
+
+                    if (northlimit == null) {
+                        northlimit = northing;
+                    }
+
+                    if (easting < westlimit)
+                        westlimit = easting;
+                    if (northing < southlimit)
+                        southlimit = northing;
+                    if (easting > eastlimit)
+                        eastlimit = easting;
+                    if (northing > northlimit)
+                        northlimit = northing;
+                }
+            }
+            
+            boundary.put("westlimit", westlimit);
+            boundary.put("southlimit", southlimit);
+            boundary.put("eastlimit", eastlimit);
+            boundary.put("northlimit", northlimit);
+        }
+    }
+}
