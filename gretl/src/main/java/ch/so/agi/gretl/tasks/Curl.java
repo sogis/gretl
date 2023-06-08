be2363b7c8c51da7dc6a39c122d360d0648e804e
Stefan Ziegler <edi.gonzales@gmail.com>
2023-06-08 11:55:11 +0200

    [CurlTask] wip

--- gretl/src/main/java/ch/so/agi/gretl/tasks/Curl.java
+++ gretl/src/main/java/ch/so/agi/gretl/tasks/Curl.java
@@ -1,7 +1,10 @@
 package ch.so.agi.gretl.tasks;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.gradle.api.DefaultTask;
@@ -12,6 +15,27 @@ import org.gradle.api.tasks.TaskAction;
 
 import ch.so.agi.gretl.logging.GretlLogger;
 import ch.so.agi.gretl.logging.LogEnvironment;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+
 
 // README.md eventuell
 // abstract class geht erst mit 5.6 oder so. Nicht mit 5.1.1
@@ -27,6 +51,9 @@ public class Curl extends DefaultTask {
     
     @Internal
     public MethodType method;
+    
+    @Internal
+    public int expectedStatusCode;
     
     @Internal
     @Optional
@@ -55,14 +82,68 @@ public class Curl extends DefaultTask {
     @Internal
     @Optional
     public String password;
-    
+       
     @TaskAction
-    public void request() {
+    public void request() throws ClientProtocolException, IOException {
         log = LogEnvironment.getLogger(Curl.class);
 
         System.out.println("*********"+serverUrl+"***********");
         System.out.println("*********"+method+"***********");
         System.out.println("*********"+formData+"***********");
+        
+        
+//        HttpRequestBase request;
+//        if (method.equals(MethodType.GET)) {
+//            request = new HttpGet(serverUrl);
+//        } else {
+//            request = new HttpPost(serverUrl);            
+//        }
+        
+        CredentialsProvider provider = null;
+        if (user != null && password != null) {
+            System.out.println(user);
+            System.out.println(password);
+            provider = new BasicCredentialsProvider();
+            provider.setCredentials(
+                    AuthScope.ANY,
+                    new UsernamePasswordCredentials(user, password)
+            ); 
+        }
+
+        MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
+        entityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+        for (Map.Entry<String, Object> entry : formData.entrySet()) {
+            if (entry.getValue() instanceof String) {
+                entityBuilder.addTextBody(entry.getKey(), (String) entry.getValue());
+            } else if (entry.getValue() instanceof File) {
+                entityBuilder.addBinaryBody(entry.getKey(), (File) entry.getValue());   
+            }
+        }
+        HttpEntity entity = entityBuilder.build();
+       
+        HttpPost request = new HttpPost(serverUrl);
+        request.setEntity(entity);
+        
+        HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+        
+        if (provider != null) {
+            System.out.println("*******asdfasd");
+            clientBuilder.setDefaultCredentialsProvider(provider);
+        }
+        
+        HttpClient httpClient = clientBuilder.build();
+
+        HttpResponse httpResponse = httpClient.execute(request);
+        System.out.println("POST Response Status:: "
+                + httpResponse.getStatusLine().getStatusCode());
+
+        
+//        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+//                CloseableHttpResponse response = httpClient.execute(post)){
+//
+//               result = EntityUtils.toString(response.getEntity());
+//           }
+
 
     }
     
