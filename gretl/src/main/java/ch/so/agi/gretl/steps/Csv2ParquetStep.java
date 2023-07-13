bd398e68b18890e194666065a1d6ee2d55e5bf52
Stefan Ziegler <edi.gonzales@gmail.com>
2023-07-13 18:37:38 +0200

    [Csv2Parquet] start with step and task

---
+++ gretl/src/main/java/ch/so/agi/gretl/steps/Csv2ParquetStep.java
@@ -0,0 +1,169 @@
+package ch.so.agi.gretl.steps;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FilenameUtils;
+
+import ch.ehi.basics.settings.Settings;
+import ch.interlis.ili2c.Ili2c;
+import ch.interlis.ili2c.Ili2cException;
+import ch.interlis.ili2c.metamodel.TransferDescription;
+import ch.interlis.ilirepository.IliManager;
+import ch.interlis.iom_j.csv.CsvReader;
+import ch.interlis.iox.IoxException;
+import ch.interlis.ioxwkf.dbtools.IoxWkfConfig;
+import ch.interlis.ioxwkf.parquet.ParquetAttributeDescriptor;
+import ch.interlis.ioxwkf.parquet.ParquetWriter;
+import ch.interlis.ili2c.config.Configuration;
+import org.interlis2.validator.Validator;
+
+import ch.interlis.iox.IoxEvent;
+import ch.interlis.iox_j.EndBasketEvent;
+import ch.interlis.iox_j.EndTransferEvent;
+import ch.interlis.iox_j.ObjectEvent;
+
+import ch.so.agi.gretl.logging.GretlLogger;
+import ch.so.agi.gretl.logging.LogEnvironment;
+
+public class Csv2ParquetStep {
+    private GretlLogger log;
+    private String taskName;
+
+    public Csv2ParquetStep() {
+        this(null);
+    }
+    
+    public Csv2ParquetStep(String taskName) {
+        if (taskName == null) {
+            this.taskName = XslTransformerStep.class.getSimpleName();
+        } else {
+            this.taskName = taskName;
+        }
+        this.log = LogEnvironment.getLogger(this.getClass());
+    }
+    
+    public void execute(Path csvPath, Path outputPath, Settings config) throws IOException {
+        log.lifecycle(String.format("Start Csv2ParquetStep(Name: %s csvPath: %s outputPath: %s config: %s)", taskName,
+                outputPath, config));
+
+        String csvBaseName = FilenameUtils.getBaseName(csvPath.getFileName().toString());
+        ParquetWriter writer = null;
+        try {
+            writer = new ParquetWriter(Paths.get(outputPath.toString(), csvBaseName + ".parquet").toFile());
+        } catch (IoxException e) {
+            throw new IOException(e.getMessage());
+        }
+        
+        CsvReader reader = null;
+        try {
+            reader = new CsvReader(csvPath.toFile(), config); // config notwendig, wegen encoding, das im Reader gesetzt wird.
+        } catch (IoxException e) {
+            throw new IOException(e.getMessage());
+        }
+        
+        boolean firstLineIsHeader = true;
+        if(config.getValue(IoxWkfConfig.SETTING_FIRSTLINE) != null) {
+            firstLineIsHeader=config.getValue(IoxWkfConfig.SETTING_FIRSTLINE).equals(IoxWkfConfig.SETTING_FIRSTLINE_AS_HEADER);
+        } 
+        reader.setFirstLineIsHeader(firstLineIsHeader);
+        log.lifecycle("first line is "+(firstLineIsHeader?"header":"data"));
+
+        String valueDelimiter=config.getValue(IoxWkfConfig.SETTING_VALUEDELIMITER);
+        if(valueDelimiter != null) {
+            reader.setValueDelimiter(valueDelimiter.charAt(0));
+            log.lifecycle("valueDelimiter <"+valueDelimiter+">.");
+        } else {
+            log.lifecycle("valueDelimiter <"+IoxWkfConfig.SETTING_VALUEDELIMITER_DEFAULT+">.");
+        }
+
+        String valueSeparator=config.getValue(IoxWkfConfig.SETTING_VALUESEPARATOR);
+        if(valueSeparator != null) {
+            reader.setValueSeparator(valueSeparator.charAt(0));
+            log.lifecycle("valueSeparator <"+valueSeparator+">.");
+        } else {
+            reader.setValueSeparator(IoxWkfConfig.SETTING_VALUESEPARATOR_DEFAULT);
+            log.lifecycle("valueSeparator <"+IoxWkfConfig.SETTING_VALUESEPARATOR_DEFAULT+">.");
+        }
+                
+        if (config.getValue(Validator.SETTING_MODELNAMES) != null) {
+            TransferDescription td = null;
+            try {
+                td = getTransferDescriptionFromModelName(config.getValue(Validator.SETTING_MODELNAMES), csvPath.getParent());
+            } catch (Ili2cException e) {
+                throw new IOException(e.getMessage());
+            }
+            reader.setModel(td);
+            writer.setModel(td);
+        }
+        
+        String[] attrs = null;
+
+        try {
+            IoxEvent event = reader.read();
+
+            while (event instanceof IoxEvent) {
+                //event = reader.read();
+                if (event instanceof ObjectEvent) {
+                    if (attrs == null) {
+                        attrs = reader.getAttributes();
+                        
+                        // Funktioniert, falls die Reihenfolge garantiert ist.
+                        // Man muss die Attribute explizit setzen. Sonst kann 
+                        // passieren, dass Attribute komplett fehlen, weil das
+                        // erste Objekt analysiert wird und einige Attribute davon
+                        // null sind und im IomObjekt nicht vorkommen.
+                        // Und funktioniert nur, falls Header-Zeile vorhanden.
+                        if (firstLineIsHeader && config.getValue(Validator.SETTING_MODELNAMES) == null) {
+                            List<ParquetAttributeDescriptor> attrDescs = new ArrayList<>();
+                            for(String attrName : attrs) {                        
+                                ParquetAttributeDescriptor attrDesc = new ParquetAttributeDescriptor();
+                                attrDesc.setAttributeName(attrName);
+                                attrDesc.setBinding(String.class);
+                                attrDescs.add(attrDesc);
+                            }
+                            writer.setAttributeDescriptors(attrDescs);                        
+                        }
+                    }                    
+                    writer.write(event);
+                }
+                event = reader.read();
+            }
+
+            writer.write(new EndBasketEvent());
+            writer.write(new EndTransferEvent());
+
+            if (writer != null) {
+                writer.close();
+                writer = null;
+            }
+            
+            if (reader != null) {
+                reader.close();
+                reader = null;
+            }
+        } catch (IoxException e) {
+            throw new IOException(e.getMessage());
+        }
+    }
+        
+    private TransferDescription getTransferDescriptionFromModelName(String iliModelName, Path additionalRepository) throws Ili2cException {
+        IliManager manager = new IliManager();        
+        String ilidirs = IoxWkfConfig.SETTING_ILIDIRS_DEFAULT + additionalRepository;
+        String repositories[] = ilidirs.split(";");
+        manager.setRepositories(repositories);
+        ArrayList<String> modelNames = new ArrayList<String>();
+        modelNames.add(iliModelName);
+        Configuration config = manager.getConfig(modelNames, 2.3);
+        TransferDescription td = Ili2c.runCompiler(config);
+
+        if (td == null) {
+            throw new IllegalArgumentException("INTERLIS compiler failed"); // TODO: can this be tested?
+        }
+        
+        return td;
+    }
+}
---
+++ gretl/src/main/java/ch/so/agi/gretl/tasks/Csv2Parquet.java
@@ -0,0 +1,109 @@
+package ch.so.agi.gretl.tasks;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.GradleException;
+import org.gradle.api.tasks.Internal;
+import org.gradle.api.tasks.Optional;
+import org.gradle.api.tasks.TaskAction;
+import org.interlis2.validator.Validator;
+
+import ch.ehi.basics.settings.Settings;
+import ch.interlis.iom_j.csv.CsvReader;
+import ch.interlis.ioxwkf.dbtools.IoxWkfConfig;
+import ch.so.agi.gretl.logging.GretlLogger;
+import ch.so.agi.gretl.logging.LogEnvironment;
+import ch.so.agi.gretl.steps.Csv2ParquetStep;
+
+public class Csv2Parquet extends DefaultTask {
+    protected GretlLogger log;
+
+    @Internal
+    public File csvFile;
+    
+    @Internal
+    @Optional
+    public boolean firstLineIsHeader = true;
+    
+    @Internal
+    @Optional
+    public Character valueDelimiter = null;
+    
+    @Internal
+    @Optional
+    public Character valueSeparator = null;
+    
+    @Internal
+    @Optional
+    public String encoding = null;
+    
+    @Internal
+    @Optional
+    public String models = null;
+    
+    @Internal
+    @Optional
+    public File configFile; // TODO siehe meta
+    
+    @Internal
+    @Optional
+    public String identifier; // TODO ohne meta auch nicht notwendig. -> OgdMetaPublisher?
+    
+    @Internal
+    @Optional
+    public File outputDir;
+    
+    @Internal
+    @Optional
+    public boolean meta = false; // TODO: wenn wir vom config file nicht mehr die CSV-Konfig brauchen, ist das jetzt redundant.
+    // TODO: oder gänzlich separat? Weil es nur "csv2parquet" ist und kein publisher.
+    
+    // TODO: nur mit normalen log-Methoden arbeiten?
+//    @Internal
+//    @Optional
+//    public boolean trace = false;
+    
+    @TaskAction
+    public void run() {
+        log = LogEnvironment.getLogger(Csv2Parquet.class);
+
+        if (csvFile == null) {
+            throw new IllegalArgumentException("csvFile must not be null");
+        }
+        
+        Settings settings = new Settings();
+        settings.setValue(IoxWkfConfig.SETTING_FIRSTLINE,
+                firstLineIsHeader ? IoxWkfConfig.SETTING_FIRSTLINE_AS_HEADER : IoxWkfConfig.SETTING_FIRSTLINE_AS_VALUE);
+        
+        if (valueDelimiter != null) {
+            settings.setValue(IoxWkfConfig.SETTING_VALUEDELIMITER, valueDelimiter.toString());
+        }
+        
+        if (valueSeparator != null) {
+            settings.setValue(IoxWkfConfig.SETTING_VALUESEPARATOR, valueSeparator.toString());
+        }
+        
+        if (encoding != null) {
+            settings.setValue(CsvReader.ENCODING, encoding);
+        }
+        
+        if (models != null) {
+            settings.setValue(Validator.SETTING_MODELNAMES, models);
+        }
+
+        if (outputDir == null) {
+            outputDir = csvFile.getParentFile();
+        }
+
+        try {
+            Csv2ParquetStep csv2ParquetStep = new Csv2ParquetStep();
+            csv2ParquetStep.execute(csvFile.toPath(), outputDir.toPath(), settings);
+            log.lifecycle("Parquet file written: " + csvFile.getAbsolutePath());
+        } catch (IOException e) {
+            throw new GradleException("Could not write parquet file: " + e.getMessage());
+        }
+    }
+
+}
