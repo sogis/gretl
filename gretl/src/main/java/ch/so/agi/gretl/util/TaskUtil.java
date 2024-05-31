c7e4915076443f61f66c104dba2b9e734f00a391
Sabrina Wullschleger <sabrina.wullschleger@ebp.ch>
2024-05-31 14:12:46 +0200

    #132: improve naming

--- gretl/src/main/java/ch/so/agi/gretl/util/TaskUtil.java
+++ gretl/src/main/java/ch/so/agi/gretl/util/TaskUtil.java
@@ -15,23 +15,22 @@ public class TaskUtil {
     /**
      * Used to Convert a thrown Exception into a GradleException. GradleException
      * must be thrown to halt the Execution of the gradle build.
-     *
      * GretlException intances are converted, all other Exceptions are wrapped
      * (nested)
      */
     public static GradleException toGradleException(Exception ex) {
-        GradleException res = null;
+        GradleException exception;
 
         String exClassName = ex.getClass().toString();
         String gretlClassName = GretlException.class.toString();
 
         if (exClassName.equals(gretlClassName)) { // can't use instanceof as must return false for GretlException
                                                   // subclasses.
-            res = new GradleException(ex.getMessage());
+            exception = new GradleException(ex.getMessage());
         } else {
-            res = new GradleException("Inner Exception Message: " + ex.getMessage(), ex);
+            exception = new GradleException("Inner Exception Message: " + ex.getMessage(), ex);
         }
-        return res;
+        return exception;
     }
 
     /**
