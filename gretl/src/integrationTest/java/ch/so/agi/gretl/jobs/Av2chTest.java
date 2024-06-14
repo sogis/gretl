0d73d253a4d92deac54b3d67b72bead871f60a58
Sabrina Wullschleger <sabrina.wullschleger@ebp.ch>
2024-06-14 14:51:38 +0200

    #135: Update Av2chTest to use BuildKit, Add new task generating plugin list

--- gretl/src/integrationTest/java/ch/so/agi/gretl/jobs/Av2chTest.java
+++ gretl/src/integrationTest/java/ch/so/agi/gretl/jobs/Av2chTest.java
@@ -8,13 +8,55 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.fs.FileUtil;
+import org.gradle.api.Project;
+import org.gradle.testfixtures.ProjectBuilder;
+import org.junit.Rule;
 import org.junit.Test;
 
 import ch.so.agi.gretl.util.GradleVariable;
 import ch.so.agi.gretl.util.IntegrationTestUtil;
+import org.junit.rules.TemporaryFolder;
+
+import org.gradle.testkit.runner.GradleRunner;
+import org.gradle.testkit.runner.BuildResult;
 
 public class Av2chTest {
+
+    @Test
+    public void transformation_Ok_with_test_kit() throws Exception{
+        File projectDir = new File(System.getProperty("user.dir") + "/src/integrationTest/jobs/Av2ch");
+        File initScript = new File(System.getProperty("user.dir") + "/src/integrationTest/jobs/init.gradle");
+        List<File> classpath = new ArrayList<>();
+        File classpathFile = new File(System.getProperty("user.dir"),"build/pluginClassPath.txt");
+        List<String> lines = Files.readAllLines(classpathFile.toPath(), StandardCharsets.UTF_8);
+        for (String line : lines) {
+            classpath.add(new File(line));
+        }
+
+        BuildResult result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withPluginClasspath(classpath)
+            .withArguments("--init-script", initScript.getAbsolutePath(), "transform")
+            .build();
+
+        File resultFile = new File(projectDir + "/output/254900.itf");
+
+        long resultSize = resultFile.length();
+        assertTrue("Size of result file is wrong.", resultSize > 580000);
+
+        String resultString = new String(Files.readAllBytes(resultFile.toPath()), StandardCharsets.ISO_8859_1);
+
+        assertThat(resultString, containsString("DM01 Interlis Converter"));
+        assertThat(resultString, containsString("MODL DM01AVCH24LV95D"));
+        assertThat(resultString, containsString("TABL LFP3Nachfuehrung"));
+        assertThat(resultString, containsString("OBJE 2540 2514 2611693.294 1233674.211 111.9 1 3 1"));
+    }
+
     @Test
     public void transformation_Ok() throws Exception {
         GradleVariable[] gvs = null;
