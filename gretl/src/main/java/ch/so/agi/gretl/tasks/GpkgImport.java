bbb94bc29fde9b4755f21c9023ee3c34f3c39d97
Daniel Kubányi <dkubanyi@gmail.com>
2024-09-02 15:44:32 +0200

    wip: connector
    Signed-off-by: Daniel Kubányi <dkubanyi@gmail.com>

--- gretl/src/main/java/ch/so/agi/gretl/tasks/GpkgImport.java
+++ gretl/src/main/java/ch/so/agi/gretl/tasks/GpkgImport.java
@@ -1,27 +1,23 @@
 package ch.so.agi.gretl.tasks;
 
-import java.io.File;
-import java.sql.SQLException;
-import java.util.List;
-
-import org.gradle.api.DefaultTask;
-import org.gradle.api.GradleException;
-import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.InputFile;
-import org.gradle.api.tasks.Optional;
-import org.gradle.api.tasks.TaskAction;
-
 import ch.ehi.basics.settings.Settings;
 import ch.interlis.ioxwkf.dbtools.Gpkg2db;
 import ch.interlis.ioxwkf.dbtools.IoxWkfConfig;
 import ch.so.agi.gretl.api.Connector;
 import ch.so.agi.gretl.logging.GretlLogger;
 import ch.so.agi.gretl.logging.LogEnvironment;
+import ch.so.agi.gretl.tasks.impl.DatabaseTask;
 import ch.so.agi.gretl.util.TaskUtil;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputFile;
+import org.gradle.api.tasks.Optional;
+import org.gradle.api.tasks.TaskAction;
 
-public class GpkgImport extends DefaultTask {
+import java.io.File;
+import java.sql.Connection;
+
+public class GpkgImport extends DatabaseTask {
     protected GretlLogger log;
-    private Connector database;
     private Object dataFile;
     private String srcTableName;
     private String dstTableName;
@@ -29,14 +25,44 @@ public class GpkgImport extends DefaultTask {
     private Integer batchSize;
     private Integer fetchSize;
 
-    @Input
-    public Connector getDatabase(){
-        return database;
+    @TaskAction
+    public void importData() {
+        log = LogEnvironment.getLogger(GpkgImport.class);
+        Connector connector = createConnector();
+
+        if (connector == null) {
+            throw new IllegalArgumentException("database must not be null");
+        }
+        if (srcTableName == null) {
+            throw new IllegalArgumentException("srcTableName must not be null");
+        }
+        if (dstTableName == null) {
+            throw new IllegalArgumentException("dstTableName must not be null");
+        }
+        if (dataFile == null) {
+            throw new IllegalArgumentException("dataFile must not be null");
+        }
+
+        Settings settings = getSettings();
+
+        File data = this.getProject().file(dataFile);
+
+        try (Connection conn = connector.connect()) {
+            Gpkg2db gpkg2db = new Gpkg2db();
+            gpkg2db.importData(data, conn, settings);
+            conn.commit();
+            // conn.rollback()
+        } catch (Exception e) {
+            log.error("failed to run GpkgImport", e);
+            throw TaskUtil.toGradleException(e);
+        }
     }
+
     @InputFile
     public Object getDataFile(){
         return dataFile;
     }
+
     @Input
     String getSrcTableName() {
         return srcTableName;
@@ -65,18 +91,6 @@ public class GpkgImport extends DefaultTask {
         return fetchSize;
     }
 
-    public void setDatabase(List<String> databaseDetails){
-        if (databaseDetails.size() != 3) {
-            throw new IllegalArgumentException("Values for db_uri, db_user, db_pass are required.");
-        }
-
-        String databaseUri = databaseDetails.get(0);
-        String databaseUser = databaseDetails.get(1);
-        String databasePassword = databaseDetails.get(2);
-
-        this.database = new Connector(databaseUri, databaseUser, databasePassword);
-    }
-
     public void setDataFile(Object dataFile) {
         this.dataFile = dataFile;
     }
@@ -101,24 +115,11 @@ public class GpkgImport extends DefaultTask {
         this.fetchSize = fetchSize;
     }
 
-    @TaskAction
-    public void importData() {
-        log = LogEnvironment.getLogger(GpkgImport.class);
-        if (database == null) {
-            throw new IllegalArgumentException("database must not be null");
-        }
-        if (srcTableName == null) {
-            throw new IllegalArgumentException("srcTableName must not be null");
-        }
-        if (dstTableName == null) {
-            throw new IllegalArgumentException("dstTableName must not be null");
-        }        
-        if (dataFile == null) {
-            throw new IllegalArgumentException("dataFile must not be null");
-        }
+    private Settings getSettings() {
         Settings settings = new Settings();
         settings.setValue(IoxWkfConfig.SETTING_GPKGTABLE, srcTableName);
         settings.setValue(IoxWkfConfig.SETTING_DBTABLE, dstTableName);
+
         // set optional parameters
         if (schemaName != null) {
             settings.setValue(IoxWkfConfig.SETTING_DBSCHEMA, schemaName);
@@ -127,36 +128,9 @@ public class GpkgImport extends DefaultTask {
             settings.setValue(IoxWkfConfig.SETTING_BATCHSIZE, batchSize.toString());
         }
         if (fetchSize != null) {
-            settings.setValue(IoxWkfConfig.SETTING_FETCHSIZE, fetchSize.toString());            
+            settings.setValue(IoxWkfConfig.SETTING_FETCHSIZE, fetchSize.toString());
         }
 
-        File data = this.getProject().file(dataFile);
-        java.sql.Connection conn = null;
-        try {
-            conn = database.connect();
-            if (conn == null) {
-                throw new IllegalArgumentException("connection must not be null");
-            }
-            
-            Gpkg2db gpkg2db=new Gpkg2db();
-            gpkg2db.importData(data, conn, settings);
-            conn.commit();
-            conn.close();
-            conn = null;
-        } catch (Exception e) {
-            log.error("failed to run GpkgImport", e);
-            GradleException ge = TaskUtil.toGradleException(e);
-            throw ge;
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.rollback();
-                    conn.close();
-                } catch (SQLException e) {
-                    log.error("failed to rollback/close", e);
-                }
-                conn = null;
-            }
-        }
+        return settings;
     }
 }
---
+++ gretl/src/main/java/ch/so/agi/gretl/tasks/impl/DatabaseTask.java
@@ -0,0 +1,52 @@
+package ch.so.agi.gretl.tasks.impl;
+
+import ch.so.agi.gretl.api.Connector;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.Optional;
+import org.gradle.api.tasks.TaskAction;
+
+public abstract class DatabaseTask extends DefaultTask {
+    private String dbUri;
+    private String dbUser;
+    private String dbPassword;
+
+    @TaskAction
+    public Connector createConnector() {
+        if (dbUri == null || dbUser == null || dbPassword == null) {
+            throw new IllegalArgumentException("dbUri, dbUser, and dbPassword must all be provided.");
+        }
+
+        return new Connector(dbUri, dbUser, dbPassword);
+    }
+
+    @Input
+    @Optional
+    public String getDbUri() {
+        return dbUri;
+    }
+
+    public void setDbUri(String dbUri) {
+        this.dbUri = dbUri;
+    }
+
+    @Input
+    @Optional
+    public String getDbUser() {
+        return dbUser;
+    }
+
+    public void setDbUser(String dbUser) {
+        this.dbUser = dbUser;
+    }
+
+    @Input
+    @Optional
+    public String getDbPassword() {
+        return dbPassword;
+    }
+
+    public void setDbPassword(String dbPassword) {
+        this.dbPassword = dbPassword;
+    }
+}
