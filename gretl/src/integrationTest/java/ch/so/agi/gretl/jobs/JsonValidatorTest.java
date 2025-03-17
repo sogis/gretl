8dc6a880cb979a6b8a5a5af2e7e559a7a0dded6c
Stefan Ziegler <edi.gonzales@gmail.com>
2025-03-17 11:49:55 +0100

    [JsonValidator] start with json validator

---
+++ gretl/src/integrationTest/java/ch/so/agi/gretl/jobs/JsonValidatorTest.java
@@ -0,0 +1,25 @@
+package ch.so.agi.gretl.jobs;
+
+import ch.so.agi.gretl.util.IntegrationTestUtil;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class JsonValidatorTest {
+//    @Test
+//    public void validationOk() throws Exception {
+//        File projectDirectory = new File(System.getProperty("user.dir") + "/src/integrationTest/jobs/JsonValidatorOk");
+//
+//        IntegrationTestUtil.executeTestRunner(projectDirectory);
+//    }
+//    @Test
+//    public void validationFail() {
+//        File projectDirectory = new File(System.getProperty("user.dir") + "/src/integrationTest/jobs/JsonValidatorFail");
+//
+//        assertThrows(Throwable.class, () -> {
+//            IntegrationTestUtil.executeTestRunner(projectDirectory);
+//        });
+//    }
+}
---
+++ gretl/src/main/java/ch/so/agi/gretl/tasks/impl/JsonValidatorImpl.java
@@ -0,0 +1,53 @@
+package ch.so.agi.gretl.tasks.impl;
+
+import java.io.File;
+
+import ch.interlis.iox_j.PipelinePool;
+import org.interlis2.validator.Validator;
+
+import ch.ehi.basics.settings.Settings;
+import ch.interlis.ili2c.metamodel.TransferDescription;
+import ch.interlis.iox.IoxException;
+import ch.interlis.iox.IoxReader;
+import ch.interlis.iox_j.logging.LogEventFactory;
+import ch.interlis.ioxwkf.dbtools.IoxWkfConfig;
+import ch.interlis.ioxwkf.json.JsonReader;
+
+public class JsonValidatorImpl extends Validator {
+
+    @Override
+    protected IoxReader createReader(String filename, TransferDescription td, LogEventFactory errFactory,
+            Settings settings, PipelinePool pool) throws IoxException {
+        JsonReader reader = new JsonReader(new File(filename), settings);
+        reader.setModel(td);
+        
+        // Ggf Umgang mit top level array.
+        
+        
+//        boolean firstLineIsHeader = false;
+//        {
+//            String val = settings.getValue(IoxWkfConfig.SETTING_FIRSTLINE);
+//            if (IoxWkfConfig.SETTING_FIRSTLINE_AS_HEADER.equals(val)) {
+//                firstLineIsHeader = true;
+//            }
+//        }
+//        reader.setFirstLineIsHeader(firstLineIsHeader);
+//        char valueDelimiter = IoxWkfConfig.SETTING_VALUEDELIMITER_DEFAULT;
+//        {
+//            String val = settings.getValue(IoxWkfConfig.SETTING_VALUEDELIMITER);
+//            if (val != null) {
+//                valueDelimiter = val.charAt(0);
+//            }
+//        }
+//        reader.setValueDelimiter(valueDelimiter);
+//        char valueSeparator = IoxWkfConfig.SETTING_VALUESEPARATOR_DEFAULT;
+//        {
+//            String val = settings.getValue(IoxWkfConfig.SETTING_VALUESEPARATOR);
+//            if (val != null) {
+//                valueSeparator = val.charAt(0);
+//            }
+//        }
+//        reader.setValueSeparator(valueSeparator);
+        return reader;
+    }
+}
